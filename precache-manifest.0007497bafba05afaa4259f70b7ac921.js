58db330e6eddf662bf45917b04e04939c7a68032
Damian <dhfxblog@gmail.com>
2020-04-20 12:32:34 +0200

    Updates

---
+++ precache-manifest.0007497bafba05afaa4259f70b7ac921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c500eb80c279fb83df1500f65e4496",
+    "url": "/Covid-in-Poland/index.html"
+  },
+  {
+    "revision": "4e42b94e573de828b563",
+    "url": "/Covid-in-Poland/static/css/main.e502bcca.chunk.css"
+  },
+  {
+    "revision": "2c66bf27cfc67b9e00a8",
+    "url": "/Covid-in-Poland/static/js/2.0b776eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-in-Poland/static/js/2.0b776eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e42b94e573de828b563",
+    "url": "/Covid-in-Poland/static/js/main.3af883c7.chunk.js"
+  },
+  {
+    "revision": "0cb70ccf2f3eff8053cd",
+    "url": "/Covid-in-Poland/static/js/runtime-main.beed408e.js"
+  },
+  {
+    "revision": "378f1d40958888b5381496cd9e87c405",
+    "url": "/Covid-in-Poland/static/media/img2.378f1d40.png"
+  }
+]);
